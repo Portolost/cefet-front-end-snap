4ea6442350dcacd253c5d8770f29b5064066ee4a
Gabriel Comonian Porto <74382419+Portolost@users.noreply.github.com>
2021-02-24 22:00:58 -0300

    end

--- balaozinho.js
+++ balaozinho.js
@@ -1,20 +1,38 @@
-function inMark(e) {
-    let clicadoEl = e.currentTarget;
-    let el = document.querySelector('#' + clicadoEl.id);
-    balaoEl.innerHTML = '<h1>' + el.dataset.titulo + '</h1><p>' + el.dataset.conteudo + '</p>';
-    balaoEl.style.display = "";
-    
-}
-
-function outMark(e) {
-    balaoEl.innerHTML = "";
-    balaoEl.style.display="";
-}
-
 const balaoEl = document.querySelector("#balaozinho");
 const arrMaksEl = document.querySelectorAll(".marcacao");
+const botaoEl = document.querySelector("#definir-marcacao");
+botaoEl.addEventListener("click", defMark);
+
+balaoEl.style.display = "none";
 
 for (markEl of arrMaksEl) {
     markEl.addEventListener("mouseover", inMark);
     markEl.addEventListener("mouseout", outMark);
+    markEl.addEventListener("mousemove", segueMouse);
+}
+
+function segueMouse(e) {
+    balaoEl.style.left = `${e.pageX}px`;
+    balaoEl.style.top = `${e.pageY}px`;
+}
+
+function inMark(e) {
+    let clicadoEl = e.currentTarget;
+    balaoEl.style.display = "";
+
+    let el = document.querySelector('#' + clicadoEl.id);
+    
+    balaoEl.innerHTML = '<h1>' + el.dataset.titulo + '</h1><p>' + el.dataset.conteudo + '</p>';
+}
+function outMark(e){
+    balaoEl.innerHTML = "";
+    balaoEl.style.display = "none";
+}
+
+function defMark(e) {
+    let x = document.querySelector('#marcacao-x');
+    let y = document.querySelector('#marcacao-y');
+    let larg = document.querySelector('#marcacao-largura');
+    let alt = document.querySelector('#marcacao-altura');
+    arrMaksEl[0].style = "width: " + larg.value + "px; height: " + alt.value + "px; top: " + y.value + "px; left: " + x.value + "px;";
 }
